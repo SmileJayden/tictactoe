a4a2acdc640e2a7ddb2e3b96df6c916c86254d64
SmileJayden <wkdwodud07@gmail.com>
2020-02-22 19:18:30 +0900

    introduce nuxtpropertydecorator

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,14 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        tictactoe
-      </h1>
-      <h2 class="subtitle">
-        My spectacular Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    Index Page
   </div>
 </template>
 
 <script lang="ts">
-import Logo from "~/components/Logo.vue";
+import { Vue, Component } from "nuxt-property-decorator";
 
-export default {
-  components: {
-    Logo
-  }
-};
+@Component
+export default class Index extends Vue {}
 </script>
 
 <style>
