623cd7abf48007dfe72126b2acf79b9e3ae3f5f1
SmileJayden <wkdwodud07@gmail.com>
2020-03-02 20:55:35 +0900

    introduce nuxtjsmoment

--- components/MyToy.vue
+++ components/MyToy.vue
@@ -8,9 +8,7 @@
 import { Vue, Component } from 'nuxt-property-decorator';
 
 @Component
-export default class MyToy extends Vue {
-  myObj = { a: 1, b: 2, c: 2, d: 2, e: 2, f: 2, g: 2, h: 2, i: 2 };
-}
+export default class MyToy extends Vue {}
 </script>
 
 <style scoped>
--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: ['@nuxt/typescript-build'],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/moment'],
   /*
    ** Nuxt.js modules
    */
