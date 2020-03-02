afa90e81a659fc14b3b0dea9a76c71c3783c85fa
SmileJayden <wkdwodud07@gmail.com>
2020-03-02 20:50:11 +0900

    add toy page

---
+++ components/MyToy.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="root">
+    Root
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class MyToy extends Vue {
+  myObj = { a: 1, b: 2, c: 2, d: 2, e: 2, f: 2, g: 2, h: 2, i: 2 };
+}
+</script>
+
+<style scoped>
+.root {
+  color: blue;
+}
+</style>
---
+++ pages/toy.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button @click="onClick">onClick</button>
+    <button @click="ArrowOnClick">ArrowOnClick</button>
+    <my-toy class="hih"></my-toy>
+    <div>
+      <div v-for="(i, j, k, l) of myObj">{{ i }} {{ j }}{{ k }}{{ l }}</div>
+    </div>
+    <div>
+      <div v-for="(i, j, k) of myArr">{{ i }} {{ j }}{{ k }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+import MyToy from '~/components/MyToy.vue';
+
+@Component({
+  components: { MyToy },
+})
+export default class toy extends Vue {
+  myObj = { a: 11, b: 22, c: 33, d: 44 };
+  myArr = [11, 22, 33, 44, 55];
+
+  onClick() {
+    console.log('this', this);
+    console.dir(this);
+    console.log('myArr', this.myArr);
+  }
+
+  ArrowOnClick = () => {
+    console.log('this', this);
+    console.dir(this);
+    console.log('myArr', this.myArr);
+  };
+}
+</script>
+
+<style scoped>
+.hih {
+  color: red;
+}
+
+button {
+  width: 200px;
+  height: 200px;
+}
+</style>
