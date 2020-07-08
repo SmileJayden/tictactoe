50df2b5cd52f34860932a672c55d3caa426c67ed
SmileJayden <wkdwodud07@gmail.com>
2020-07-08 19:56:27 +0900

    introduce node sass

--- pages/infinite.vue
+++ pages/infinite.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    sakdfjlasdj
+  <div class="container">
+    <div
+      class="row"
+      v-for="(item, i) in items"
+      :style="{ 'background-color': item.color }"
+    >
+      <p>{{ i }} 번째</p>
+      <div>{{ item.name }}</div>
+      <div>{{ item.desc }}</div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +16,35 @@
 import { Vue, Component } from 'nuxt-property-decorator';
 
 @Component
-export default class Infinite extends Vue {}
+export default class Infinite extends Vue {
+  items = [this.getnerateItem(), this.getnerateItem()];
+
+  getnerateItem(): object {
+    const color: string = '#' + ((Math.random() * 0xffffff) << 0).toString(16);
+    return {
+      name: color,
+      desc: 'Math.random()',
+      color: color,
+    };
+  }
+}
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.container {
+  text-align: center;
+  padding: 30px;
+  margin: 100px;
+  border: 20px solid darkkhaki;
+  height: 500px;
+  .row {
+    height: 150px;
+    display: flex;
+    justify-content: space-evenly;
+    margin-bottom: 30px;
+    align-items: center;
+    color: white;
+    font-size: 30px;
+  }
+}
+</style>
