b4c726dd25dd8f3252e06b86fa4ccdc79f1b3334
SmileJayden <wkdwodud07@gmail.com>
2020-07-08 19:43:46 +0900

    init infinite page

---
+++ pages/infinite.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    sakdfjlasdj
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class Infinite extends Vue {}
+</script>
+
+<style scoped></style>
